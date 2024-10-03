d4f417810a5908ea9cb0c6cd46fdeef8bb4c5f31
Andres Rivero <andres19rivero@gmail.com>
2024-10-02 23:47:26 -0400

    configure formatting by ft

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -147,6 +147,32 @@ return {
       },
     },
   },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+			formatters_by_ft = {
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				javascriptreact = { "prettier" },
+				typescriptreact = { "prettier" },
+				svelte = { "prettier" },
+				css = { "prettier" },
+				html = { "prettier" },
+				json = { "prettier" },
+				yaml = { "prettier" },
+				markdown = { "prettier" },
+				graphql = { "prettier" },
+				liquid = { "prettier" },
+				lua = { "stylua" },
+				python = { "isort", "black" },
+			},
+			-- format_on_save = {
+			-- 	lsp_fallback = true,
+			-- 	async = false,
+			-- 	timeout_ms = 1000,
+			-- },
+		}
+  },
 
   -- since `vim.tbl_deep_extend`, can only merge tables and not lists, the code above
   -- would overwrite `ensure_installed` with the new value.
