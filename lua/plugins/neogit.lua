b0fe715d70248fe0cd8745132c8a250ea182aba4
Andres Rivero <andres19rivero@gmail.com>
2025-09-17 13:36:29 -0400

    feat: add lazygit plugin

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -1,21 +1,42 @@
 return {
-  "NeogitOrg/neogit",
-  dependencies = {
-    "nvim-lua/plenary.nvim",         -- required
-    "sindrets/diffview.nvim",        -- optional - Diff integration
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",         -- required
+      "sindrets/diffview.nvim",        -- optional - Diff integration
 
-    -- Only one of these is needed.
-    "nvim-telescope/telescope.nvim", -- optional
-    "ibhagwan/fzf-lua",              -- optional
-    "echasnovski/mini.pick",         -- optional
+      -- Only one of these is needed.
+      "nvim-telescope/telescope.nvim", -- optional
+      "ibhagwan/fzf-lua",              -- optional
+      "echasnovski/mini.pick",         -- optional
+    },
+    opts = {},
+    config = function()
+      local neogit = require('neogit')
+      neogit.setup({})
+      local keymap = vim.keymap
+
+      keymap.set('n', "<leader>ln", neogit.open, { desc = "Open Neogit" })
+    end
   },
-  opts = {},
-  config = function()
-    local neogit = require('neogit')
-    neogit.setup({})
-    local keymap = vim.keymap
-
-    keymap.set('n', "<leader>ln", neogit.open, { desc = "Open Neogit" })
-  end
+  {
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+    }}
 }
 
